66ca22d098a66d4a6fc9234a82bcb00117d198cd
Amin Bihamta <aminbihamtawork@gmail.com>
2026-08-20 14:43:37 +0800

    First commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,67 +1,41 @@
-import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert h-5 w-[100px]"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the{" "}
-            <code className="rounded bg-black/[.06] px-1.5 py-0.5 font-mono text-[0.9em] dark:bg-white/[.08]">
-              page.tsx
-            </code>{" "}
-            file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
+      <main className="">
+        <div className="mb-80">
+          <h1 className="text-4xl text-center">Welcome to Motif</h1>
+          <h2 className="text-2xl text-center">Your vibe finder</h2>
+
+          <div className="flex w-fill justify-center gap-4 mt-10">
+            <Link className="border px-4 py-2 rounded-lg text-sm" href="/demo">
+              Watch a demo
+            </Link>
+            <Link
+              className="border px-4 py-2 rounded-lg text-sm"
+              href="/signIn"
             >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+              Find my vibe
+            </Link>
+          </div>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert h-[14px] w-4"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={14}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+
+        <div className="w-screen bg-zinc-800 h-[35vh] absolute bottom-0 left-0 p-5 flex justify-center items-top gap-2 flex-wrap overflow-clip">
+          <div className="w-full flex justify-center gap-2 py">
+            <div className="w-full h-auto aspect-square rounded-lg bg-zinc-700"></div>
+            <div className="w-full h-auto aspect-square rounded-lg bg-zinc-700"></div>
+            <div className="w-full h-auto aspect-square rounded-lg bg-zinc-700"></div>
+            <div className="w-full h-auto aspect-square rounded-lg bg-zinc-700"></div>
+            <div className="w-full h-auto aspect-square rounded-lg bg-zinc-700"></div>
+          </div>
+          <div className="w-full flex justify-center gap-2 py">
+            <div className="w-full h-auto aspect-square rounded-lg bg-zinc-700 ml"></div>
+            <div className="w-full h-auto aspect-square rounded-lg bg-zinc-700"></div>
+            <div className="w-full h-auto aspect-square rounded-lg bg-zinc-700"></div>
+            <div className="w-full h-auto aspect-square rounded-lg bg-zinc-700"></div>
+            <div className="w-full h-auto aspect-square rounded-lg bg-zinc-700"></div>
+          </div>
         </div>
       </main>
     </div>
