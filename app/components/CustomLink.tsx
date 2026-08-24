7a89a2c2779fe06167d9e2d7079adf863873a5ef
Amin Bihamta <aminbihamtawork@gmail.com>
2026-08-25 01:39:07 +0800

    Sign in page improvements

--- app/components/CustomLink.tsx
+++ app/components/CustomLink.tsx
@@ -7,13 +7,14 @@ interface LinkProps {
   icon: React.ComponentType;
   link: string;
   icon_left: boolean;
+  custom_className?: string;
 }
 
 
-export default function CustomLink({theme, label, icon_left, icon: Icon, link}: LinkProps) {
+export default function CustomLink({theme,custom_className, label, icon_left, icon: Icon, link}: LinkProps) {
     return (
       <Link
-        className={`border px-4 py-2 cursor-pointer rounded-lg text-sm hover:text-motif-charcoal  flex flex-row gap-1 justify-center items-center transition-colors duration-300 ease-in-out hover:border-motif-ivory hover:animate-[gap-pulse_1s_ease-in-out_infinite] ${theme === "cream" ? "hover:bg-motif-ivory bg-transparent" : ""} ${theme === "red" ? "hover:bg-motif-ivory bg-motif-red border-motif-red hover:text-motif-red" : ""} ${theme === "charcol" ? "text-motif-charcoal hover:text-motif-ivory border-motif-charcoal hover:bg-motif-charcoal hover:border-motif-charcoal bg-motif-charcol border-motif-charcol hover:text-motif-charcol" : ""}`}
+        className={` ${custom_className} border px-4 py-2 cursor-pointer rounded-lg text-sm hover:text-motif-charcoal  flex flex-row gap-1 justify-center items-center transition-colors duration-300 ease-in-out hover:border-motif-ivory hover:animate-[gap-pulse_1s_ease-in-out_infinite] ${theme === "cream" ? "hover:bg-motif-ivory bg-transparent" : ""} ${theme === "red" ? "hover:bg-motif-ivory bg-motif-red border-motif-red hover:text-motif-red" : ""} ${theme === "charcol" ? "text-motif-charcoal hover:text-motif-ivory border-motif-charcoal hover:bg-motif-charcoal hover:border-motif-charcoal bg-motif-charcol border-motif-charcol hover:text-motif-charcol" : ""}`}
         href={link}
       >
 
--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -1,8 +1,7 @@
 import Form from "next/form";
 import AnimatedContainer from "../components/animated-container";
-import { GoogleCircle, ArrowRight } from "iconoir-react/regular";
-import CustomLink from "../components/CustomLink";
 import CustomButton from "../components/CustomButton";
+import Link from "next/link"
 
 export default function SignIn() {
   return (
@@ -15,22 +14,31 @@ export default function SignIn() {
           <h1 className="text-center text-6xl text-motif-charcoal">
             Welcome back
           </h1>
-          <span>Sign in to continue discovering</span>
-          <CustomLink
-            label="Sign in with Google"
-            link=""
-            icon={GoogleCircle}
-            theme="charcol"
-            icon_left={true}
-          />
-          <div className="flex flex-row justify-center items-center border-motif-charcoal gap-2 w-20 py-6">
+          <span className="text-motif-charcoal mb-10 mt-2">Sign in to continue discovering</span>
+
+          <Link
+            className={`border px-4 py-2 cursor-pointer rounded-lg text-sm hover:text-motif-charcoal  flex flex-row gap-1 justify-center items-center transition-colors duration-300 ease-in-out hover:border-motif-ivory hover:animate-[gap-pulse_1s_ease-in-out_infinite] text-motif-charcoal hover:text-motif-ivory border-motif-charcoal hover:bg-motif-charcoal hover:border-motif-charcoal bg-motif-charcol border-motif-charcol hover:text-motif-charcol w-full gap-2`}
+            href=""
+          >
+
+                  <svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48" className="block w-4">
+                        <path fill="#EA4335" d="M24 9.5c3.54 0 6.71 1.22 9.21 3.6l6.85-6.85C35.9 2.38 30.47 0 24 0 14.62 0 6.51 5.38 2.56 13.22l7.98 6.19C12.43 13.72 17.74 9.5 24 9.5z"></path>
+                        <path fill="#4285F4" d="M46.98 24.55c0-1.57-.15-3.09-.38-4.55H24v9.02h12.94c-.58 2.96-2.26 5.48-4.78 7.18l7.73 6c4.51-4.18 7.09-10.36 7.09-17.65z"></path>
+                        <path fill="#FBBC05" d="M10.53 28.59c-.48-1.45-.76-2.99-.76-4.59s.27-3.14.76-4.59l-7.98-6.19C.92 16.46 0 20.12 0 24c0 3.88.92 7.54 2.56 10.78l7.97-6.19z"></path>
+                        <path fill="#34A853" d="M24 48c6.48 0 11.93-2.13 15.89-5.81l-7.73-6c-2.15 1.45-4.92 2.3-8.16 2.3-6.26 0-11.57-4.22-13.47-9.91l-7.98 6.19C6.51 42.62 14.62 48 24 48z"></path>
+                        <path fill="none" d="M0 0h48v48H0z"></path>
+                      </svg>
+                Sign in with Google
+
+          </Link>
+          <div className="flex flex-row justify-center items-center border-motif-charcoal gap-2 w-43 py-6">
             <div className="flex min-w-full bg-motif-charcoal h-px -mb-1"></div>
             <span className="text-motif-charcoal flex w-fit">or</span>
             <div className="flex min-w-full bg-motif-charcoal h-px -mb-1"></div>
           </div>
           <Form
             action="/signin"
-            className="flex flex-col gap-2 w-full max-w-80 items-end  text-motif-charcoal"
+            className="flex flex-col gap-2 max-w-80 items-end  text-motif-charcoal w-full max-w-full"
           >
             <label className="flex w-full flex-col">
               <span>Email</span>
@@ -59,7 +67,7 @@ export default function SignIn() {
               icon_left={false}
               custom_className="w-full"
               />
-            <p className="text-center w-full">New to Motif? <span className="text-motif-red hover:underline cursor-pointer">Create an account!</span></p>
+            <p className="text-center w-full mt-4">New to Motif? <span className="text-motif-red hover:underline cursor-pointer">Create an account!</span></p>
 
           </Form>
         </div>
